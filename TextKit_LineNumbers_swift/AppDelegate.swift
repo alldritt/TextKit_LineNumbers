87aa0318a3a6595ba7b1377027859e025658b9aa
Mickaël Floc’hlay <mickael.flochlay@memo.bank>
2021-01-26 14:04:57 +0100

    chore: update code to support Swift 5 and Xcode 12.x

--- TextKit_LineNumbers_swift/AppDelegate.swift
+++ TextKit_LineNumbers_swift/AppDelegate.swift
@@ -9,13 +9,10 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
-  var window: UIWindow?
-
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-    return true
-  }
-
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
 }
-
